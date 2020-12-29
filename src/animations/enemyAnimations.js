7e99c337e7391a43ccc3c0fe92273f47bf5a871d
gelacio <gbsf.software.dev@gmail.com>
2020-12-29 17:25:35 -0600

    Se organizaron las  varibales para que tengan igual orden

--- src/animations/enemyAnimations.js
+++ src/animations/enemyAnimations.js
@@ -2,28 +2,28 @@ import Phaser from '../lib/phaser.js'
 
 export const createEnemyAnimations = (anims) =>{
     anims.create({
-        key: 'enemyleft',
-        frames: anims.generateFrameNumbers('player', { frames: [21, 22, 21, 34]}),
+        key: 'ENEMYLEFT',
+        frames: anims.generateFrameNumbers('player', { frames: [22, 28, 22, 34]}),
         frameRate: 10,
         repeat: -1
     });
 
     anims.create({
-        key: 'enemyright',
+        key: 'ENEMYRIGHT',
         frames: anims.generateFrameNumbers('player', { frames: [22, 28, 22, 34] }),
         frameRate: 10,
         repeat: -1
     });
 
     anims.create({
-        key: 'enemyup',
+        key: 'ENEMYUP',
         frames: anims.generateFrameNumbers('player', { frames: [23, 29, 23, 35]}),
         frameRate: 10,
         repeat: -1
     });
 
     anims.create({
-        key: 'enemydown',
+        key: 'ENEMYDOWN',
         frames: anims.generateFrameNumbers('player', { frames: [21, 27, 21, 33] }),
         frameRate: 10,
         repeat: -1
--- src/game/Enemy.js
+++ src/game/Enemy.js
@@ -14,7 +14,7 @@ class Enemy extends Phaser.Physics.Arcade.Sprite
     constructor(scene, x, y, texture, frame){
         super(scene, x, y, texture, frame);
         this.setScale(1);
-        this.anims.play('enemyright', true);
+        this.anims.play(`ENEMY${Directions.RIGHT}`, true);
         scene.physics.world.on('worldbounds', this.changeDirection, this);
         scene.time.addEvent({
             delay:2000,
@@ -27,17 +27,23 @@ class Enemy extends Phaser.Physics.Arcade.Sprite
         super.preUpdate(t, dt);
 
         switch(this.direction){
+            case Directions.LEFT:
+                this.anims.play(`ENEMY${Directions.LEFT}`, true);
+                this.body.setVelocityX(-this.SPEED);
+                this.flipX = true;
+                break;
+            case Directions.RIGHT:
+                this.anims.play(`ENEMY${Directions.RIGHT}`, true);
+                this.body.setVelocityX(this.SPEED);
+                this.flipX = false;
+                break;
             case Directions.UP:
+                this.anims.play(`ENEMY${Directions.UP}`, true);
                 this.body.setVelocityY(-this.SPEED);
                 break;
             case Directions.DOWN:
+                this.anims.play(`ENEMY${Directions.DOWN}`, true);
                 this.body.setVelocityY(this.SPEED);
-                break;
-            case Directions.LEFT:
-                this.body.setVelocityX(-this.SPEED);
-                break;
-            case Directions.RIGHT:
-                this.body.setVelocityX(this.SPEED);
                 break;
         }
     }
