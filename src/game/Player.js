1808169a94987f625853366328606dff3b16695d
gelacio <gbsf.software.dev@gmail.com>
2020-12-29 16:53:35 -0600

    Se quitaron comentarios de la seccion de mapas

--- src/game/Player.js
+++ src/game/Player.js
@@ -8,7 +8,6 @@ const  Directions = {
     LEFT:"LEFT",
     RIGHT:"RIGHT",
 }
-
 class Player extends Phaser.Physics.Arcade.Sprite
 {
     constructor(scene, x, y, texture, frame){
--- src/scenes/WorldScene.js
+++ src/scenes/WorldScene.js
@@ -18,13 +18,9 @@ class WorldScene extends Phaser.Scene{
     create(){
         // --------- MAP
         var map = this.make.tilemap({ key: 'map' });
-        // first parameter is the name of the tilemap in tiled
         var tiles = map.addTilesetImage('spritesheet', 'tiles',16,16,1,2);
-        // creating the layers
         var grass = map.createStaticLayer('Grass', tiles, 0, 0);
         var obstacles = map.createStaticLayer('Obstacles', tiles, 0, 0);
-
-        // make all tiles in obstacles collidable
         obstacles.setCollisionByExclusion([-1]);    
 
         // --------- PLAYER
